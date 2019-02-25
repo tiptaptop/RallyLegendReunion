2ad5885b8dc616ba3a4addb4f89328ed36a6ce89
Dieu <boblool31@gmail.com>
2019-02-25 16:05:00 +0400

    Ajout de "page-top"
    Rajout du bandeau et du menu en haut de page

--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -2,6 +2,32 @@
 
 @section('contenu')
 
+<body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-shrink" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" id="index">Rally Legend</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" style="Color: #D35352;" href="index2.html">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" style="Color: #D35352;" href="focus.html">essaie/focus</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" style="Color: #D35352;" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
 	<section class="bg-light" id="portfolio">
     	<div class="container">
       		<div class="row">
