6c8588a1b8d607e5ee03c9b6268832fa44b03458
DrrSchultz <boblool31@gmail.com>
2019-05-03 11:00:08 +0400

    galerie photo
    2ème partie

--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -30,39 +30,6 @@
 
 
 
-  <!-- Modal -->
-  <div class="portfolio-modal modal fade" id="portfolioModal12" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="close-modal" data-dismiss="modal">
-          <div class="lr">
-            <div class="rl"></div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 mx-auto">
-              <div class="modal-body">
-                <!-- Project Details Go Here -->
-                <h2 class="text-uppercase"></h2>
-                <p class="item-intro text-muted"></p>
-                <img class="img-fluid d-block mx-auto" src="" alt="">
-                <p></p>
-                <ul class="list-inline">
-                  <li>Date: </li>
-                  <li>Évènenement: </li>
-                  <li>Catégorie: </li>
-                </ul>
-                <button class="btn btn-primary" data-dismiss="modal" type="button">
-                  <i class="fas fa-times"></i>
-                  Fermer la fenêtre</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
     <br>
     <div class="col-sm-offset-4 col-sm-4">
