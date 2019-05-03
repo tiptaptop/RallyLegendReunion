80f2c2dd6d7c6c53701c410f0ae745ede7c3b579
DrrSchultz <boblool31@gmail.com>
2019-05-03 14:30:59 +0400

    1.5

--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -16,14 +16,14 @@
     <!-- Page Content -->
    <div class="container page-top">
 
+
+
         <div class="row">
-
-
             <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a href="https://images.pexels.com/photos/62307/air-bubbles-diving-underwater-blow-62307.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="fancybox" rel="ligthbox">
                     <img  src="https://images.pexels.com/photos/62307/air-bubbles-diving-underwater-blow-62307.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" class="zoom img-fluid "  alt="">
                 </a>
             </div>
-
        </div>
 		</div>
 
